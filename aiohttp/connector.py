2584137dc908965ee3c8b109bd578fb3597a6771
J. Nick Koston <nick@koston.org>
2024-10-29 17:52:57 -1000

    preen

--- aiohttp/connector.py
+++ aiohttp/connector.py
@@ -1439,7 +1439,10 @@ class UnixConnector(BaseConnector):
         return self._path
 
     async def _create_connection(
-        self, req: ClientRequest, traces: List["Trace"], timeout: "ClientTimeout"
+        self,
+        req: ClientRequest,
+        traces: Optional[List["Trace"]],
+        timeout: "ClientTimeout",
     ) -> ResponseHandler:
         try:
             async with ceil_timeout(
@@ -1501,7 +1504,10 @@ class NamedPipeConnector(BaseConnector):
         return self._path
 
     async def _create_connection(
-        self, req: ClientRequest, traces: List["Trace"], timeout: "ClientTimeout"
+        self,
+        req: ClientRequest,
+        traces: Optional[List["Trace"]],
+        timeout: "ClientTimeout",
     ) -> ResponseHandler:
         try:
             async with ceil_timeout(
